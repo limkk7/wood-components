1ce2fee0366bcde84b04c7a96388bfd116f9ba79
linpeng <versionlin@foxmail.com>
2021-10-31 11:54:35 +0800

    组件导出

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,10 +1,1 @@
-import ReactDOM from "react-dom";
-import React from "react";
-import Icon from "./icon/icon";
-const fn: React.MouseEventHandler = (e) => {};
-ReactDOM.render(
-  <div>
-    <Icon name="wood" onClick={fn} />
-  </div>,
-  document.querySelector("#root")
-);
+export { default as Icon } from "./icon/icon";
