d94f315088b612f2d85a397d58113c3ff3312aa4
linpeng <versionlin@foxmail.com>
2021-11-02 09:09:22 +0800

    update

---
+++ .history/lib/button/button.example_20211102085316.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button } from "wood-ui";
+
+const ButtonExample: React.FC = () => {
+  return <Button>hihihi</Button>;
+};
+
+export default ButtonExample;
---
+++ .history/lib/example_20211102042447.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter as Router, Route, Link, NavLink } from "react-router-dom";
+import IconExample from "./icon/icon.example";
+import ButtonExample from "./button.example";
+import LayoutExample from "./layout/layout.example";
+import { Layout, Header, Aside, Content, Footer, Icon } from "./index";
+import DialogDemo from "./dialog/dialog.demo";
+import "./example.scss";
+
+ReactDOM.render(
+  <Router>
+    <Layout className="index-layout">
+      <Header className="header">
+        <Link to="/">
+          <Icon name="wood" className="logo" />
+        </Link>
+      </Header>
+      <Layout className="main">
+        <Aside className="aside">
+          <h2>组件</h2>
+          <ul>
+            <li>
+              <NavLink to="/icon">Icon</NavLink>
+            </li>
+            <li>
+              <NavLink to="/button">Button</NavLink>
+            </li>
+            <li>
+              <NavLink to="/dialog">Dialog</NavLink>
+            </li>
+            <li>
+              <NavLink to="/layout">layout</NavLink>
+            </li>
+          </ul>
+        </Aside>
+        <Content className="content">
+          <Route path="/icon" component={IconExample} />
+          <Route path="/button" component={ButtonExample} />
+          <Route path="/dialog" component={DialogDemo} />
+          <Route path="/layout" component={LayoutExample} />
+        </Content>
+      </Layout>
+      <Footer className="footer">
+        <ul>
+          <li>
+            <Icon name="wood" className="logo" />
+          </li>
+          <li>
+            <a
+              href="https://github.com/versionlin7/wood-components"
+              target="__blank"
+            >
+              <Icon name="github" className="github" />
+            </a>
+          </li>
+        </ul>
+      </Footer>
+    </Layout>
+  </Router>,
+  document.querySelector("#root")
+);
